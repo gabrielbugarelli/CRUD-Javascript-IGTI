1f925863ad4854c2ab76342540549cb597c33d0c
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-03-10 12:45:57 -0400

    Implementando Funcionalidade de Alterar o campo.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 window.addEventListener('load', start);
 
 let globalNames = ['Um', 'Dois', 'Três', 'Quatro']
-
 let inputName = null;
+let isEditing = false;
+let currentIndex = null;
+
+
 
 function start() {
     inputName = document.querySelector("#inputName");
@@ -28,13 +31,32 @@ function activateInput() {
     function insertName(newName) {
         globalNames.push(newName)
         console.log(globalNames)
-        render();
+    }
+
+    function updateName(newName) {
+        globalNames[currentIndex] = newName
     }
 
     function handleTyping(event) {
+
+        let hasText = !!event.target.value && event.target.value.trim() !== '';
+        if (!hasText) {
+            clearInput()
+            return
+        }
+
         if (event.key === 'Enter') {
-            let typedName = event.target.value
-            insertName(typedName)
+            if (isEditing) {
+                updateName(event.target.value)
+                console.log('editing')
+            } else {
+                insertName(event.target.value)
+                console.log('inserting')
+            }
+
+            render();
+            isEditing = false;
+            clearInput();
         }
     }
 
@@ -54,10 +76,28 @@ function render() {
         let button = document.createElement('button')
         button.classList.add('deleteButton')
         button.textContent = 'x'
-
         button.addEventListener('click', deleteName)
 
         return button;
+    }
+
+    function createSpan(name, index) {
+
+        function editItem() {
+            inputName.value = name
+            inputName.focus()
+            isEditing = true;
+
+            currentIndex = index
+        }
+
+        let span = document.createElement('span')
+        span.classList.add('clickable')
+        span.textContent = name
+        span.addEventListener('click', editItem)
+
+        return span;
+
     }
 
     let divNames = document.querySelector("#names")
@@ -70,9 +110,7 @@ function render() {
 
         let li = document.createElement('li')
         let button = createDeleteButton(i)
-
-        let span = document.createElement('span')
-        span.textContent = currentName
+        let span = createSpan(currentName, i)
 
         li.appendChild(button)
         li.appendChild(span)
