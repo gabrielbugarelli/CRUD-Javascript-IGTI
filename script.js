dbc28e81d73ea779dd45fd9d3e792eb823920435
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-03-16 15:29:30 -0400

    Refatoração do Script.js

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-window.addEventListener('load', start);
-
 let globalNames = ['Um', 'Dois', 'Três', 'Quatro']
 let inputName = null;
 let isEditing = false;
 let currentIndex = null;
 
-
-
-function start() {
+window.addEventListener('load', () => {
     inputName = document.querySelector("#inputName");
 
     preventFormSubmit();
     activateInput();
 
     render();
-
-}
+});
 
 function preventFormSubmit() {
     function handleFormSubmit(event) {
@@ -29,7 +24,8 @@ function preventFormSubmit() {
 function activateInput() {
 
     function insertName(newName) {
-        globalNames.push(newName)
+        // globalNames.push(newName)
+        globalNames = [...globalNames, newName]
         console.log(globalNames)
     }
 
@@ -65,11 +61,12 @@ function activateInput() {
 }
 
 function render() {
-
     function createDeleteButton(index) {
 
         function deleteName() {
-            globalNames.splice(index, 1)
+            //globalNames.splice(index, 1)
+            globalNames = globalNames.filter((_, i) => i !== index);
+
             render()
         }
 
@@ -121,7 +118,7 @@ function render() {
     clearInput()
 }
 
-function clearInput() {
+const clearInput = () => {
     inputName.value = ''
     inputName.focus()
 }
